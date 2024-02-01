eadbb07efef5b400493b77e5717c2a378593d612
Seongwon <sngoneee@gmail.com>
2024-02-01 18:36:59 +0900

    0201 study

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 
 
+
+
 (() => {
 
     let yOffset = 0;    //window.pageYOffset 대신 쓸 변수
     let prevScrollHeight = 0;  //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0; //현재 활성화된 (눈 앞에 보고있는) 씬 (scroll-section)
-
-
 
     const sceneInfo = [
         {
@@ -49,37 +49,66 @@
     ];
 
 
+
+
     // 각 스크롤 섹션의 높이 세팅
     function setLayout() {
-		for (let i = 0; i < currentScene; i++) {
+		for (let i = 0; i < sceneInfo.length; i++) {
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
-        console.log(sceneInfo);
+
+
+        // 자동으로 currentScene 확인
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= pageYOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+
+
+
     }
+
+
 
 
 
     function scrollLoop() {
-        prevScrollHeight = 0;
-        for(let i = 0; i < sceneInfo.length; i++) {
+        prevScrollHeight = 0; 
+        for(let i = 0; i < currentScene; i++) {
             prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
         }
-        console.log("prevScrollHeight "+ prevScrollHeight);
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+        if (yOffset < prevScrollHeight) {
+            
+            // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지 (모바일)
+            if (currentScene == 0) {
+                return;
+            }
+            currentScene--;
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`)
     }
 
 
 
-    window.addEventListener('resize', setLayout);
+
+
+
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
     });
 
-
-    setLayout();
-
-
+    window.addEventListener('load', setLayout);
+    // = window.addEventListener('DOMContentloaded', setLayout)
+    window.addEventListener('resize', setLayout);
 
 }) ();
  
